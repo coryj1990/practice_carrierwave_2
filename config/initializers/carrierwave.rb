200ad49d40fcae0feac500c329910852376426a8
Cory Johnson <cory.r.johnson@cox.net>
2016-08-25 14:28:45 -0500

    dotenv is used to solve constant issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ require 'pry'
 CarrierWave.configure do |config|
 	binding.pry
   	config.fog_credentials = {
-    	:provider               => 'AWS',                        
-    	:aws_access_key_id      => AWS_ACCESS_KEY                        
-    	:aws_secret_access_key  => AWS_SECRET_KEY                      
+    	:provider               => 'AWS',                       
+    	:aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        
+    	:aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                      
   	}
-  	config.fog_directory  = AWS_BUCKET                     
-end
+  	config.fog_directory  = ENV["AWS_BUCKET"]                     
+end	
