2166be01a81559db9751c49a97fe028bdde2e498
Cory Johnson <cory.r.johnson@cox.net>
2016-08-29 08:58:41 -0500

    Switched to psql cuz heroku don'lik'it

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160825153246) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "videos", force: :cascade do |t|
     t.string   "video_file"
     t.string   "name"
